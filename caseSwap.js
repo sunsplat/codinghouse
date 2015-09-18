1d420d065e454f8a591673f9f2b8770e07b73e2c
sunsplat <heeycutie@yahoo.com>
2015-09-18 12:48:04 +0800

    first commit

---
+++ caseSwap.js
@@ -0,0 +1,16 @@
+function swapCase(str) {
+	  // var strArray = str.split('');
+    var newArray = '';
+   // for (var i = 0; i < strArray.length; i++) {
+    for (var i = 0; i < str.length; i++) {
+    	if (str[i] === str[i].toUpperCase()) {
+          newArray += str[i].toLowerCase();
+          } else {
+          newArray += str[i].toUpperCase();
+          }
+    }
+  console.log(newArray);
+  //return newArray;
+}
+
+swapCase("hEllO wOrld");
